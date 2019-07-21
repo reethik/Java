72ca55131fd8728298d1391e9fa50a833ca112e8
Reethik.Thota <52414342+reethik@users.noreply.github.com>
2019-07-21 15:38:03 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,18 @@
+package add;
+import java.util.Scanner;
+
+public class apples {
+
+    public static void main(String args[]){
+        Scanner bucky = new Scanner(System.in);
+        double fnum, snum, answer;
+        System.out.println("Enter first num: ");
+        fnum = bucky.nextDouble();
+        System.out.println("Enter second num: ");
+        snum = bucky.nextDouble();
+        answer = fnum + snum;
+        System.out.println(answer);
+    }
+}
+
+
