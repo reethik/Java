23bfa51d6f735c676b9996a936add672443af433
Reethik.Thota <52414342+reethik@users.noreply.github.com>
2019-07-23 21:14:32 +0530

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,33 @@
+public class BasicArithmeticDemo
+{
+ 
+    public static void main(String[] args)
+    {
+        int number1 = 10;
+        int number2 = 5;
+ 
+        //calculating number1 + number2;
+        int sum = number1 + number2;
+ 
+        //calculating number1 - number2;
+        int difference = number1 - number2;
+ 
+        //calculating number1 * number2;
+        int product = number1 * number2;
+ 
+        //calculating number1 / number2;
+        int quot = number1 / number2;
+ 
+        //calculating number1 % number2;
+        int rem = number1 % number2;
+ 
+        //Displaying the values
+        System.out.println("number1 : "+number1);
+        System.out.println("number2 : "+number2);
+        System.out.println("sum : "+sum);
+        System.out.println("difference : "+difference);
+        System.out.println("product : "+product);
+        System.out.println("quot : "+quot);
+        System.out.println("rem : "+rem);
+    }
+}
